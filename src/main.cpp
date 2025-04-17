5bc79a2420c5083958067eb8509f43a4ebea0b53
chavand.aurelien <chavand.aurelien@gmail.com>
2025-04-17 15:55:39 +0200

    Commite #1 Conf et test de l'ecrant

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,38 @@
 #include <Arduino.h>
+#include "Wire.h"
+#include "SSD1306.h"
 
 // put function declarations here:
 int myFunction(int, int);
 
+SSD1306 ecranOled(0x3c, 5, 4); 
+
 void setup() {
+
   // put your setup code here, to run once:
+  /*
   Serial.begin(115200);
-  
+
   int result = myFunction(2, 3);
+ */
+
+  // put your setup code here, to run once:
+   // Initialisation du système graphique
+ecranOled.init();
+// Nettoyage de l'écran
+ecranOled.clear();
+
+// Définition de la police
+ecranOled.setFont("Arial");
+
+// Affichage du texte de présentation
+ecranOled.drawString(1, 2, "Nom de l'application : ArduinoJson");
+ecranOled.drawString(2, 4, "Version : 7.2.0");
+
+// Rafraîchissement de l'affichage
+ecranOled.display();
+
+
 }
 
 void loop() {
